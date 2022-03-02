8d3b41ce3d95fe05e057c0c135a8c1f4e0339fae
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2022-03-02 13:14:52 -0500

    Updated index.html test

--- tests/Homepage_test.py
+++ tests/Homepage_test.py
@@ -8,16 +8,5 @@ def test_request_main_menu_links(client):
     assert b'<li class="nav-item"><a class="nav-link" href="/about">About</a></li>' in response.data
 
 
-def test_request_index(client):
-    """This checks the About option on navbar"""
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b"About" in response.data
-
-def test_request_about(client):
-    """This checks the Home option on navbar"""
-    response = client.get("/about")
-    assert response.status_code == 200
-    assert b"Home" in response.data
 
 
