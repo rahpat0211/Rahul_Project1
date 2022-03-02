3b0a5cd75841ab6bfa23decdb208ac25c9128a1e
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2022-03-02 14:13:56 -0500

    Updated aboutpage test

--- tests/Aboutpage_test.py
+++ tests/Aboutpage_test.py
@@ -4,7 +4,7 @@ def test_request_about(client):
     """This checks the title of the web page"""
     response = client.get("/about")
     assert response.status_code == 200
-    assert b"<title>Rahul's F-Layou Page</title>" in response.data
+    assert b"<title>Rahul's F-Layout Page</title>" in response.data
 
 def test_request_to_do_list(client):
     """This checks for the to do list"""
