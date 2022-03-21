16d8818e3d410e0ac3da9de15e6ad7f108bb93f0
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2022-03-21 11:09:04 -0400

    Updated calculator

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,59 +3,37 @@
 from calculator import Calculator
 
 
-# this defines a test must say test_
-def test_calculator_is_instance():
-    """Testing the Calculator"""
-    # instantiating the Calculator class
-    calculator = Calculator()
-    # checking that calculator variable contains an instance of Calculator class
-    assert isinstance(calculator, Calculator)
-
-
-def test_calculator_get_result_method():
-    """Testing the Calculator"""
-    calculator = Calculator()
-    # this checks the calculator get result method
-    assert calculator.get_result() == 0
-
-
-def test_calculator_result_property():
-    """Testing the Calculator"""
-    #  these show multiple calculator classes being instantiated and used independently
-    calc1 = Calculator()
-    calc2 = Calculator()
-    calc1.result = 5
-    calc2.result = 6
-    assert calc1.result == 5
-    assert calc2.result == 6
+def tuple_list():
+    """Arranging Data for AAA Testing"""
+    return 1.0, 2
 
 
 def test_calculator_add_method():
     """Testing the Calculator"""
-    calculator = Calculator()
     # this is show using the calculator object add method
-    assert calculator.add(1, 1) == 2
+
+    ## Act for AAA testing
+    result = Calculator.add(tuple_list())
+
+    ## Assertion for AAA testing
+    assert result == 3
 
 
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
-    calculator = Calculator()
-    assert calculator.subtract(1, 1) == 0
+    assert Calculator.subtract(tuple_list()) == -3
 
 
 def test_calculator_multiply_method():
     """Testing the Calculator Subtract"""
-    calculator = Calculator()
-    assert calculator.multiply(1, 1) == 1
+    assert Calculator.multiply(tuple_list()) == 2
 
 
-def test_my_first_test_add():
-    """Testing the simplest addition"""
-    assert 1 + 1 == 2
+def test_calculator_add_method():
+    """Testing the Calculator Add"""
+    assert Calculator.add(tuple_list()) == 3
 
-def test_my_first_test_add_with_variables():
-    """Testing the simplest addition"""
-    value_a = 1
-    value_b = 2
-    value_c = value_a + value_b
-    assert value_a + value_b == value_c
+
+def test_calculator_divide_method():
+    """Testing the Calculator Divide"""
+    assert Calculator.divide(tuple_list()) == 0
